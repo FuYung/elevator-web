913877ca16d19d2aa36f59ba4b4f6d582bb11b62
FuYung <fuyungcn@gmail.com>
2015-09-29 10:44:13 +0800

    initial commit

--- app/config.router.js
+++ app/config.router.js
@@ -4,24 +4,46 @@ app.config(['$stateProvider', function ($stateProvider) {
     $stateProvider
         .state('index', {
             url: '/index',
-            templateUrl: '../home/home.html',
-            ncyBreadcrumb: {
-                label: '首页'
-            }
+            templateUrl: 'home/home.html'
         })
         .state('login', {
             url: '/login',
             templateUrl: 'login/login.html',
-            controller: 'LoginCtrl',
-            ncyBreadcrumb: {
-                label: '登录'
-            }
+            controller: 'LoginCtrl'
         })
         .state('index.brand', {
             url: '/brand',
-            templateUrl: 'brand/brand.html',
-            ncyBreadcrumb: {
-                label: '品牌管理'
-            }
+            templateUrl: 'brand/brand.html'
+        })
+
+        .state('index.elevator', {
+            url: '/elevator',
+            templateUrl: 'elevator/elevator.html'
+        })
+
+        .state('index.user', {
+            url: '/user',
+            templateUrl: 'user/user.html'
+        })
+        .state('index.docs', {
+            url: '/docs',
+            templateUrl: 'docs.html'
+        })
+        .state('signin', {
+            url: '/signin',
+            templateUrl: 'login/login.html'
+        })
+
+        .state('index.uiTree', {
+            url: '/ui_tree',
+            templateUrl: 'other/ui_tree.html'
+        })
+        .state('index.uiChart', {
+            url: '/ui_chart',
+            templateUrl: 'other/ui_chart.html'
+        })
+        .state('index.uiWidgets', {
+            url: '/ui_widgets',
+            templateUrl: 'other/ui_widgets.html'
         })
 }]);
